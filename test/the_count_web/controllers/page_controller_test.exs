0a92bd1c514f7ad51e22b7b20368b4e19b50625f
Nathan Long <him@nathanmlong.com>
2018-08-31 16:54:33 -0400

    Fix dummy test

--- test/the_count_web/controllers/page_controller_test.exs
+++ test/the_count_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule TheCountWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "The counter is..."
   end
 end
